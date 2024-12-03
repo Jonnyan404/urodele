681e648e2e18d2eece067d94893f4719f1316d6c
jonny <coorhook@gmail.com>
2024-12-03 20:28:53 +0800

    Update urodele.config.ts

--- urodele.config.ts
+++ urodele.config.ts
@@ -1,7 +1,7 @@
 export const config = {
   github: {
-    login: "glink25", // github login name, not user name
-    repo: "test-for-anything", //"urodele",
+    login: "jonnyan404", // github login name, not user name
+    repo: "jonnyan404.github.io", //"urodele",
     logInUrl: "",
     logInAuthUrl: "",
   },
